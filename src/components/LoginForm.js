ce1d91060b886709bab5eac64804a37d0e8ce56d
billyott <willrichott@gmail.com>
2020-11-18 21:09:39 -0500

    done

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -4,28 +4,45 @@ class LoginForm extends React.Component {
   constructor() {
     super();
 
-    this.state = {};
+    this.state = {
+      username: '',
+      password: ''
+    };
+  }
+
+  updateFormData = (e) => {
+    this.setState({
+      [e.target.name]: e.target.value
+    })
+  }
+
+  submitFormData = (e) => {
+    e.preventDefault()
+
+    if (!this.state.username || !this.state.password) return
+
+    this.props.handleLogin(this.state)
   }
 
   render() {
     return (
-      <form>
-        <div>
-          <label>
-            Username
-            <input id="username" name="username" type="text" />
-          </label>
-        </div>
-        <div>
-          <label>
-            Password
-            <input id="password" name="password" type="password" />
-          </label>
-        </div>
-        <div>
-          <button type="submit">Log in</button>
-        </div>
-      </form>
+      <form onSubmit={this.submitFormData}>
+      <div>
+        <label>
+          Username
+          <input id="username" name="username" type="text" onChange={this.updateFormData} value={this.state.username}/>
+        </label>
+      </div>
+      <div>
+        <label>
+          Password
+          <input id="password" name="password" type="password" onChange={this.updateFormData} value={this.state.password}/>
+        </label>
+      </div>
+      <div>
+        <button type="submit">Log in</button>
+      </div>
+    </form>
     );
   }
 }
--- src/components/TwitterMessage.js
+++ src/components/TwitterMessage.js
@@ -4,14 +4,25 @@ class TwitterMessage extends React.Component {
   constructor() {
     super();
 
-    this.state = {};
+    this.state = {
+      message: ''
+    };
+  }
+
+  tweeting = (e) => {
+    this.setState({message: e.target.value})
+  }
+
+  charsLeft = () => {
+    return this.props.maxChars - this.state.message.length
   }
 
   render() {
     return (
       <div>
         <strong>Your message:</strong>
-        <input type="text" name="message" id="message" />
+        <input type="text" name="message" id="message" onChange={this.tweeting} value={this.state.message} />
+        <p>{this.charsLeft()}</p>
       </div>
     );
   }
